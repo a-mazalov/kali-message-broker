0d70b3d371877b1b6586e10edaffef3850885592
Мазалов Александр Максимович <413664@kali.by>
2025-01-28 11:50:51 +0300

    feat: add MotherDayStatus

---
+++ src/Messages/Data/BaseStatus.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kali\MessageBroker\Messages\Data;
+
+use Kali\MessageBroker\Messages\Data\Base;
+
+class BaseStatus extends Base
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $body,
+        public string $username,
+        public string $status,
+    ) {
+    }
+
+    public function toResource(): array
+    {
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "body" => $this->body,
+            "username" => $this->username,
+            "status" => $this->status,
+        ];
+    }
+
+    public static function from(string|array $data)
+    {
+        $params = self::prepareParamsFrom($data);
+
+        return new self(
+            id: $params->id,
+            title: $params->title,
+            body: $params->body,
+            username: $params->username,
+            status: $params->status,
+        );
+    }
+}
---
+++ src/Messages/Data/Statements/MotherDayStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kali\MessageBroker\Messages\Data\Statements;
+
+use Kali\MessageBroker\Messages\Data\BaseStatus;
+
+class MotherDayStatus extends BaseStatus
+{
+    protected string $template  = "mother-day-status";
+}
